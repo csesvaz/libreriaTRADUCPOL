2ddce68f0f35fa443ee980df3c95dca10a365488
Csesvaz <camilocococo@gmail.com>
2023-05-18 21:01:09 +0200

    inicio

---
+++ TraducpolLibreria/src/main/java/es/mdef/TraducpolLibreria/TraducpolLibreriaApplication.java
@@ -0,0 +1,13 @@
+package es.mdef.TraducpolLibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraducpolLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraducpolLibreriaApplication.class, args);
+	}
+
+}
---
+++ TraducpolLibreria/src/test/java/es/mdef/TraducpolLibreria/TraducpolLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mdef.TraducpolLibreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraducpolLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
